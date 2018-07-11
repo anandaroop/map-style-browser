67be1bddee1bab6e769627fbdc093c665d7c3062
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2018-07-11 00:35:07 -0400

    Add documentation

--- src/components/ContentMenu.tsx
+++ src/components/ContentMenu.tsx
@@ -2,12 +2,18 @@ import * as React from 'react'
 import styled from '../styles/styled-components'
 
 export enum ContentType {
+  /** Basic point and label features */
   simple = 'Simple locator',
+
+  /** Areal features */
   entities = 'Historical entities',
+
+  /** Miscellaneous spatial features such as trade routes, invasions */
   phenomena = 'Historical phenomena'
 }
 
 export interface IContentProps {
+  /** The style of information overlaid on the basemap */
   contentStyle: ContentType
 }
 
@@ -15,6 +21,11 @@ export const ContentMenu = ({ contentStyle }: IContentProps) => (
   <Div>
     <h2>Choose a content style</h2>
     <p>{contentStyle}</p>
+    <ul>
+      <li>{ContentType.simple}</li>
+      <li>{ContentType.entities}</li>
+      <li>{ContentType.phenomena}</li>
+    </ul>
   </Div>
 )
 
--- src/components/VisualMenu.tsx
+++ src/components/VisualMenu.tsx
@@ -2,12 +2,18 @@ import * as React from 'react'
 import styled from '../styles/styled-components'
 
 export enum VisualType {
+  /** Full-color land cover relief background */
   natural = 'Natural',
+
+  /** Monochrome shaded relief background */
   relief = 'Relief',
+
+  /** Flat tint background */
   flat = 'Flat'
 }
 
 export interface IVisualProps {
+  /** The style of the basemap */
   visualStyle: VisualType
 }
 
@@ -15,6 +21,11 @@ export const VisualMenu = ({ visualStyle }: IVisualProps) => (
   <Div>
     <h2>Choose a visual style</h2>
     <p>{visualStyle}</p>
+    <ul>
+      <li>{VisualType.natural}</li>
+      <li>{VisualType.relief}</li>
+      <li>{VisualType.flat}</li>
+    </ul>
   </Div>
 )
 
