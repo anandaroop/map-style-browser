14a4918ed0bcbcf95cd0f9b8255e00ac4cc4271f
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2018-07-13 21:51:45 -0400

    Make a theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,29 @@
-export default interface IThemeInterface {
-  color: string
+export interface IThemeInterface {
+  font: {
+    family: string
+    weight: {
+      bold: number
+      normal: number
+    }
+  }
+  spacing: {
+    half: string
+    single: string
+    double: string
+  }
+}
+
+export const theme: IThemeInterface = {
+  font: {
+    family: "'Helvetica Neue', sans-serif",
+    weight: {
+      bold: 500,
+      normal: 300
+    }
+  },
+  spacing: {
+    half: '0.5em',
+    single: '1em',
+    double: '2em'
+  }
 }
